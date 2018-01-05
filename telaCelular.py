cbf506bceeeaff2d235540426ec58ddad94640c3
Marcelo Ramos Jordao <marcelo.rjordao@icloud.com>
2018-01-05 00:17:15 -0200

    Update telaCelular.py

--- telaCelular.py
+++ telaCelular.py
@@ -4,14 +4,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 
 
-class Ex20(BoxLayout):
+class Leitor(BoxLayout):
     def on_touch_down(self, touch):
         print("pos: {}\nspos: {}\n".format(touch.pos, touch.spos))
 
 
-class Ex20App(App):
+class LeitorApp(App):
     def build(self):
         return Ex20()
 
 if __name__ == '__main__':
-    Ex20App().run()
+    LeitorApp().run()
